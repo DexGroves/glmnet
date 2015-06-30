b04866005bb62ceb0b8ebac9820ea96a6f6fceee
DexGroves <dex.groves@gmail.com>
2015-06-30 17:33:44 +0100

    Clause in predict.elnet

--- R/predict.elnet.R
+++ R/predict.elnet.R
@@ -1,3 +1,7 @@
 predict.elnet=function(object,newx,s=NULL,type=c("link","response","coefficients","nonzero"),exact=FALSE,offset,...){
+  # Provision for one-row prediction on numeric
+  if (class(newx) != "matrix"){
+    newx <- t(as.matrix(newx))
+  }
 NextMethod("predict")
  } 
